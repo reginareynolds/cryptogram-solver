4b388b54fe3b255c05b842a7f78a95266082bb3d
Regina Reynolds <regina.c.reynolds@gmail.com>
2022-10-31 17:22:24 -0400

    Remove Tkinter sections

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import os
 import time
 from functools import partial
 from threading import Thread
-from tkinter.filedialog import askopenfilename
+
+# from wordfreq import word_frequency
+
+from idna import check_hyphen_ok
 from patterns import get_word_pattern
 from word_patterns import dictionary_patterns
 from copy import deepcopy
@@ -17,128 +20,6 @@ from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
-
-class Menu(Tk):
-    def __init__(self, title, buttons):
-        # Final variables
-        self.path = None
-        self.encoded = Cryptogram()
-
-        # CREATE MENU ITEMS
-
-        # CONFIGURE MENU
-        Tk.__init__(self)
-        # Bind keys to functions
-        self.bind('<Return>', self.onclick)
-
-        # Set window title
-        self.title(title)
-        self.frame = Frame(self)
-
-        # Create buttons
-        self.buttons = buttons  # Button click history
-        self.rows = []  # Array to store button locations
-        index = 2
-        for button in self.buttons:
-            self.button = Button(
-                self, text=button[1], command=lambda response=button[0]: self.onclick(response))
-            self.button.grid(row=index, column=0, padx=50,
-                             pady=5, sticky="W E")
-            self.rows.append(self.button)
-            index = index + 1
-
-            # Disable buttons until file is selected
-            if index-3 == 0:
-                pass
-            else:
-                self.button['state'] = 'disabled'
-
-        # Return to menu or close program
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
-
-    # Enable submission on selection of file
-    def enable(self):
-        # File has been selected
-        if self.path != None:
-            for button in self.rows:
-                button['state'] = 'normal'
-        else:  # File not selected yet
-            self.button['state'] = 'disabled'
-
-    def onclick(self, response):
-        # User selected button with mouse
-        if type(response) == int:
-            pass
-        # User selected button with keyboard
-        else:
-            index = 1
-            for location in self.rows:
-                if self.focus_get() == location:
-                    response = index
-                else:
-                    pass
-                index = index + 1
-
-        choice = 'func_' + str(response)
-        method = getattr(self, choice)
-        return method()
-
-    # Enable menu option opening and closing
-    @staticmethod
-    def hide(frame):
-        frame.withdraw()
-
-    @staticmethod
-    def show(frame):
-        frame.update()
-        frame.deiconify()
-
-    # File prompt
-    def func_1(self):
-        # Hide menu
-        self.hide(self)
-
-        # Create prompt for file
-        self.path = file_prompt()
-
-        # Check if user closed out of file prompt
-        if self.path == '':
-            self.path = None  # Reset path
-            self.rows[0].config(text=self.buttons[0][1])  # Reset button text
-        else:
-            # Change button text to selected path
-            self.rows[0].config(text=self.path)
-
-        self.enable()
-        self.show(self)
-
-    # Begin decryption
-    def func_2(self):
-        # Hide menu
-        self.hide(self)
-
-        # Set path to cryptogram file and open
-        self.encoded.file = self.path
-        self.encoded.parse()
-        self.enable()
-        self.show(self)
-        # TODO: Show decrypted text in new window
-        # TODO: Only run decryption once to prevent additional parsing unless file path changes
-
-    def on_closing(self):
-        ans = messagebox.askokcancel(
-            'Verify exit', "Do you really want to quit the program?")
-        if ans:
-            self.quit()
-            sys.exit(3)
-        else:
-            pass
-
-
-def file_prompt():
-    Tk().withdraw()
-    filename = askopenfilename(filetypes=[("Text files", "*.txt")])
-    return filename
 
 
 # Find common potential decryption values
